a3f0309d9268dfa7b4ae46ecada51e0390ff014d
Teammid <ja.natthakorn_st@tni.ac.th>
2022-01-26 17:13:53 +0700

    Part

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View,TouchableOpacity,Image } from 'react-native';
-import FirstPage from './component/pages/firstpage';
-import SecondPage from './component/pages/secondpage';
-import ThirdPage from './component/pages/thirdpage';
+import {  View,TouchableOpacity } from 'react-native';
+import FirstPage from './component/pages/FirstPage';
+import SecondPage from './component/pages/SecondPage';
+import ThirdPage from './component/pages/ThirdPage';
 import {createDrawerNavigator} from "@react-navigation/drawer"
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+
+const NavigationDrawerStructure = (props) => {
+  const toggleDrawer = () => {
+    props.NavigationProps.toggleDrawer();
+  }
+  return(
+    <View style={{flexDirection:"row"}}>
+      <TouchableOpacity onPress={() => toggleDrawer()}>
+      <MaterialCommunityIcons style={{marginLeft: 20}} name="menu" size={26} color="white" />
+      </TouchableOpacity>
+    </View>
+  )
+
+}
 
 function firstScreenStack({navigation}){
   return(
@@ -17,10 +31,17 @@ function firstScreenStack({navigation}){
           screenOptions = {{
             headerStyle:{backgroundColor: "#5f9ea0"},
             headerTintColor:"white",
-            headerTitleStyle:{fontWeight:"bold"},
-            headerLeft: () => <NavigationDrawerStructure navigationProps={navigation} />
+            headerTitleSstyle:{fontWeight:"bold"},
+            headerLeft: () => <NavigationDrawerStructure NavigationProps={navigation} />
           }}>
-          <Stack.Screen name='FirstPage' component={FirstPage}/>
+          <Stack.Screen
+          name='FirstPage' 
+          component={FirstPage}
+          />
+          <Stack.Screen
+          name="ThirdPage"
+          component={ThirdPage}
+      />
         </Stack.Navigator>
   )
 }
@@ -32,47 +53,38 @@ function seccondScreenStack({navigation}){
       headerStyle:{backgroundColor: "#5f9ea0"},
       headerTintColor:"white",
       headerTitleStyle:{fontWeight:"bold"},
-      headerLeft: () => <NavigationDrawerStructure navigationProps={navigation} />
+      headerLeft: () => <NavigationDrawerStructure NavigationProps={navigation} />
     }}>
-    <Stack.Screen name='SecondPage' component={SecondPage}/>
-    <Stack.Screen name='ThirdPage' component={ThirdPage}/>
+    <Stack.Screen 
+    name='SecondPage' 
+    component={SecondPage}
+    />
   </Stack.Navigator>
   )
 }
 
-const NavigationDrawerStructure = (props) => {
-
-  const toggleDrawer = () => {
-    props.navigationProps.toggleDrawer();
-  }
-
-  return(
-    <View style={{flexDirection:"row"}}>
-      <TouchableOpacity onPress={() => toggleDrawer()}>
-      <MaterialCommunityIcons style={{marginLeft: 20}} name="menu" size={26} color="white" />
-      </TouchableOpacity>
-    </View>
-  )
-
-}
-
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text>This is Navigation Class NAJA</Text> */}
-      <NavigationContainer>
-        <Drawer.Navigator>
-          <Drawer.Screen name='FirstDrawerPage' component={firstScreenStack}/> 
-          <Drawer.Screen name='SecondDrawerPage' component={seccondScreenStack}/>
-        </Drawer.Navigator>
-      </NavigationContainer>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Drawer.Navigator
+    drawerContentOptions={{
+      activeTintColor:'#e91263',
+      itemStyle:{marginVertical:5}
+    }}
+    >
+      <Drawer.Screen
+        name="FirstPage"
+        component={firstScreenStack}
+        option={{title: 'First Page'}}
+      />
+      <Drawer.Screen
+        name="SecondPage"
+        component={seccondScreenStack}
+        option={{title: 'Second Page'}}
+      />
+    </Drawer.Navigator>
+  </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-});
+ 
