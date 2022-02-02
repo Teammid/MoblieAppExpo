3963d1fe352154ac55d5637ab3f7cef9db9806ac
Teammid <94981835+Teammid@users.noreply.github.com>
2022-02-02 11:42:35 +0700

    Part

--- App.js
+++ App.js
@@ -1,87 +1,156 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View, TouchableOpacity } from 'react-native';
-import FirstPage from './component/pages/FirstPage';
-import SecondPage from './component/pages/SecondPage';
-import ThirdPage from './component/pages/ThirdPage';
-import { createDrawerNavigator } from "@react-navigation/drawer"
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import { View, Image } from 'react-native';
+
+import FirstPage from './pages/FirstPage';
+import SecondPage from './pages/SecondPage';
+import ThirdPage from './pages/ThirdPage';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import CustomSidebarMenu from './pages/CustomSidebarMenu';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
-const NavigationDrawerStructure = (props) => {
-  const toggleDrawer = () => {
-    props.NavigationProps.toggleDrawer();
+const NavigationDrawerStructure = (props)=>{
+  //define Structure for navigation Drawer
+  const toggleDrawer = () =>{
+    props.navigationProps.toggleDrawer();
   }
-  return (
-    <View style={{ flexDirection: "row" }}>
-      <TouchableOpacity onPress={() => toggleDrawer()}>
-        <MaterialCommunityIcons style={{ marginLeft: 20 }} name="menu" size={26} color="white" />
+
+  return(
+    <View style = {{flexDirection: 'row'}}>
+      <TouchableOpacity onPress={()=> toggleDrawer()}>
+        <Image 
+          source={require('./asset/drawerWhite.png')}
+          style={{width: 25, height: 25, marginLeft: 5}}
+       />
       </TouchableOpacity>
+
     </View>
   )
 }
-function firstScreenStack({ navigation }) {
-  return (
-    <Stack.Navigator initialRouteName="FirstPage"
+
+function FirstScreenStack({navigation}){
+  return(
+    <Stack.Navigator
+      initialRouteName= 'FirstPage'
       screenOptions={{
-        headerStyle: { backgroundColor: "#5f9ea0" },
-        headerTintColor: "white",
-        headerTitleSstyle: { fontWeight: "bold" },
-        headerLeft: () => <NavigationDrawerStructure NavigationProps={navigation} />
-      }}>
-      <Stack.Screen
-        name='FirstPage'
+        headerStyle:{backgroundColor: '#f4511e'}, 
+        headerTintColor: '#FFFF', 
+        headerTitleStyle: {fontWeight: 'bold'}, 
+      headerLeft: ()=> <NavigationDrawerStructure navigationProps={navigation}/>
+    }}
+    >
+      <Stack.Screen 
+        name= 'FirstPage' 
         component={FirstPage}
+        options={{title: 'FIRST PAGE'}}
       />
     </Stack.Navigator>
-  )
+  );
 }
 
-function seccondScreenStack({ navigation }) {
-  return (
-    <Stack.Navigator initialRouteName="FirstPage"
+function SecondScreenStack({navigation}){
+  return(
+    <Stack.Navigator
+      initialRouteName= 'FirstPage'
       screenOptions={{
-        headerStyle: { backgroundColor: "#5f9ea0" },
-        headerTintColor: "white",
-        headerTitleStyle: { fontWeight: "bold" },
-        headerLeft: () => <NavigationDrawerStructure NavigationProps={navigation} />
-      }}>
-      <Stack.Screen
-        name='SecondPage'
+        headerStyle:{backgroundColor: '#f4511e'}, 
+        headerTintColor: '#FFFF', 
+        headerTitleStyle: {fontWeight: 'bold'},
+        headerLeft: ()=> <NavigationDrawerStructure navigationProps={navigation}/>
+      }}
+    >
+      <Stack.Screen 
+        name= 'SecondPage' 
         component={SecondPage}
+        options={{title: 'SECOND PAGE'}}
       />
-      <Stack.Screen
-        name="ThirdPage"
+      <Stack.Screen 
+       name= 'ThirdPage' 
         component={ThirdPage}
+        options={{title: 'THIRD PAGE'}}
       />
     </Stack.Navigator>
-  )
+  );
 }
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator
         drawerContentOptions={{
-          activeTintColor: '#e91263',
-          itemStyle: { marginVertical: 5 }
-        }}
+          activeTintColor: '#e91e63',
+          ItemStyle: {marginVertical: 5}
+      }}
+      drawerContent = {(props)=> <CustomSidebarMenu{...props}/>}
+
       >
-        <Drawer.Screen
-          name="FirstPage"
-          component={firstScreenStack}
-          option={{ title: 'First Page' }}
-        />
-        <Drawer.Screen
-          name="SecondPage"
-          component={seccondScreenStack}
-          option={{ title: 'Second Page' }}
-        />
+        <Drawer.Screen name='FirstPage' component ={FirstScreenStack}/>
+        <Drawer.Screen name='SecondPage' component ={SecondScreenStack}/>
       </Drawer.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App;
+
+// import * as React from 'react';
+// import { Text, View } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Home!</Text>
+//     </View>
+//   );
+// }
+
+// function SettingsScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//     </View>
+//   );
+// }
+
+// const Tab = createBottomTabNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator
+//       screenOptions={({route})=>({
+//         tabBarIcon :({focused,color})=>{
+//           let iconName;
+//           if(route.name === 'Home'){
+//             iconName = focused 
+//             ?'ios-information-circle' 
+//             : 'ios-information-circle-outline';
+//           }else if (route.name === 'Settings'){
+//           iconName = focused
+//           ?'ios-list-box'
+//           :'ios-list';
+//           }
+//           return <Ionicons name = {iconName} color = {color}/>
+//         }
+//       })}
+//       tabBarOptions = {{
+//         activeTintColor:'tomato',
+//         inactiveTintColor : 'gray'
+//       }}
+//       >
+//         <Tab.Screen name="Home" component={HomeScreen} />
+//         <Tab.Screen name="Settings" component={SettingsScreen} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
 
