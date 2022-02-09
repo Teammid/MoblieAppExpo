0fcfad954d55b0641c74f6f47ab58c51266a0e55
Teammid <94981835+Teammid@users.noreply.github.com>
2022-02-09 12:08:42 +0700

    9-2-2565 Part 2

---
+++ Screen/Screen/Register.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Register = () => {
+  return (
+    <View>
+      <Text>ลงทะเบียน</Text>
+    </View>
+  );
+};
+export default Register;
